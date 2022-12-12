de8ca2abc6db7fae5e9caf81fded6b34ed08b026
michalskimateusz <michalskimateusz@outlook.com>
2022-12-12 13:25:36 +0100

    Product Card

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,7 +1,17 @@
-const Product = () => {
+import Link from "next/link";
+import Image from "next/image";
+import { urlFor } from "../lib/client";
+
+const Product = ({product: {image, name, slug, price}}) => {
   return (
     <div>
-      Product
+      <Link href={`/product/${slug.current}`}>
+        <div className="product-card">
+          <Image src={urlFor(image && image[0]).url()} width={250} height={250} className='product-image'/>
+          <p className="product-name">{name}</p>
+          <p className="product-price">${price}</p>
+        </div>
+      </Link>
     </div>
   );
 };
